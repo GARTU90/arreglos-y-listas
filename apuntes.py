952b1f88c001b6e9f15ea95e6a79abfbc0a2fb5b
GARTU <juan.daniel.rangel.avila@gmail.com>
2023-10-30 19:30:15 +0000

    ejercicio y apuntes

---
+++ apuntes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#by gartujuan@gmail.com
+#license GNU/GPL
+#2023/10/30
+#POSICIONES ESPECIFICAS
+import numpy as np
+lista=[12,34,56,78,90,2,7,9,5,6,7,0]
+print(lista[0:3])
+print(lista[-5:])
+print(lista[-6:3])
+print(lista[::2])
+array=np.array([12,34,56,78,90,2,7,9,5,6,7,0])
+print("array")
+print(array[::2])
+#428
+#97
+#205
+#131
+#155
+print()
---
+++ ejercicios.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#by gartujuan@gmail.com
+#license GNU/GPL
+#2023/10/30
+#POSICIONES ESPECIFICAS
+lista=[12,34,56,78,90,2,7,9,5,6,7,0]
+#ejercicio  23 primeros 3 elementos
+print(lista[0:3])
+# ejercicio 24 ultimos 5
+print(lista[-5:])
+#  ejercicio especial
+print("yo pase al pizarron")
+#ejercicio 27 desviacion estandar
+import math as m
+prom=(446+213+50+389+347)/6
+desv=m.sqrt((m.pow(446-prom,2)+m.pow(213-prom,2)+m.pow(50-prom,2)+m.pow(389-prom,2)+m.pow(247,2))/6)
+print(desv)
+
